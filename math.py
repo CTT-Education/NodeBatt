de43212476164230b529f917a530e9b874babe82
thetsunamisam <thetsunamisam@gmail.com>
2022-06-17 16:48:33 -0400

    really just added this to make the new branch

--- math.py
+++ math.py
@@ -1,4 +1,17 @@
 from numpy import *
 import os
 class Calculate():
-    def __init__(self, self.)
+    def __init__(self):
+        pass
+    def remaining():
+        '''Ve = -0.0916 * T + Vs
+            Where: Vs is Starting voltage
+                   Ve is Ending voltage
+                   T is time in hours
+            Ve and Vs should both serve as input variables
+            while T should be returned for the timer function
+                   
+
+
+                   '''
+
